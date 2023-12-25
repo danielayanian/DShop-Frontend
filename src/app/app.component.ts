915d3b15f3f7f37bc376f598c509d1bcec75bc94
Daniel Ayanian <48890153+danielayanian@users.noreply.github.com>
2023-12-25 14:52:19 -0300

    creacion de components crear-cuenta, footer y login, y rutas en navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CrearCuentaComponent } from './crear-cuenta/crear-cuenta.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    /*{path:"", pathMatch: 'full', redirectTo: 'inicio'},
+    path:"inicio", component:AppComponent},*/
+    {path:"crear-cuenta", component:CrearCuentaComponent},
+    {path:"login", component:LoginComponent}
+];
